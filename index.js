0b2a91572a516eca3e3cdd858b91a9e2a8a31800
amankumarverma <theamankumarverma@gmail.com>
2023-05-05 11:22:45 +0530

    fixed the touch problem with chat-gpt

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ function myFunction() {
       moreText.style.display = "inline";
     }
   }
+
+  var pressTimer;
+
+function startPressTimer() {
+  pressTimer = window.setTimeout(function() {
+    document.body.style.webkitUserSelect = 'text';
+    document.body.style.mozUserSelect = 'text';
+    document.body.style.msUserSelect = 'text';
+    document.body.style.userSelect = 'text';
+  }, 1000);
+}
+
+function cancelPressTimer() {
+  window.clearTimeout(pressTimer);
+}
+
+var elements = document.querySelectorAll('.selectable');
+
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener('mousedown', startPressTimer);
+  elements[i].addEventListener('touchstart', startPressTimer);
+  elements[i].addEventListener('mouseup', cancelPressTimer);
+  elements[i].addEventListener('touchend', cancelPressTimer);
+}
